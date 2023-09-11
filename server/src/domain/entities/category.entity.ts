d2d9b0cfa6c2b92a449414c4c5053e9388434122
Franco Gilli <francogilli10@gmail.com>
2023-09-11 20:25:11 -0300

    Tarea3

---
+++ server/src/domain/entities/category.entity.ts
@@ -0,0 +1,17 @@
+class Category{
+    private id:string;
+    private name:string;
+    private color:string;
+
+    private constructor(
+        id:string,
+        name:string,
+        color:string,
+    ){
+        this.id=id;
+        this.name=name;
+        this.color=color;
+    }
+}
+
+export default Category;
