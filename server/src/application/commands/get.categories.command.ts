11c2470cf74f9993f1dacd530b1a832adc7402dd
Agustin Camejo <agustincamejo03@gmail.com>
2023-09-27 10:54:19 -0300

    finish tarea11

---
+++ server/src/application/commands/get.categories.command.ts
@@ -0,0 +1,19 @@
+export class getCategoriesCommand {
+    private readonly name:string;
+    private readonly color:string;
+
+    constructor(
+        name: string,
+        color: string,
+    ){
+        this.name = name;
+        this.color = color;
+    }
+
+    getName(): string{
+        return this.name;
+    }
+    getColor(): string{
+        return this.color;
+    }
+}
