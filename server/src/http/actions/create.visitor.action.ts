7603467abd0a470d21c4a83437c18477ba84f8a1
Omar Moyano <omarmoyano7w@gmail.com>
2023-10-04 11:48:06 -0300

    refactor visitor

--- server/src/http/actions/create.visitor.action.ts
+++ server/src/http/actions/create.visitor.action.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from 'express';
-import { CreateVisitorCommand } from '../../application/commands/create.visitor.command';
+import CreateVisitorCommand from '../../application/commands/create.visitor.command';
 import createVisitorgHandler from '../../application/handlers/create.visitor.handler';
 
 class CreateVisitorAction {
   async run(req: Request, res: Response) {
-    const { owner, passengers, accomodation, from, to } = req.body;
+    const { ipId, nickname} = req.body;
 
     try {
-      const command = new CreateVisitorCommand(owner, passengers, accomodation, from, to);
+      const command = new CreateVisitorCommand(ipId, nickname);
       await createVisitorgHandler.execute(command);
 
       return res.status(201).json({ message: 'Visitor created successfully' });
     } catch (error) {
       const { message } = error as Error;
-      res.status(400).json({ message: message });
+      return res.status(400).json({ message: message });
     }
   }
 }
--- server/src/http/routes/visitor.routes.ts
+++ server/src/http/routes/visitor.routes.ts
@@ -1,8 +1,6 @@
 import { Application } from 'express';
 import CommonRoutes from './common.routes';
 import createVisitorAction from '../actions/create.visitor.action';
-import createVisitorCommand  from '../../application/commands/create.visitor.command';
-import createVisitorHandler  from '../../application/handlers/create.visitor.handler';
 
 class VisitorRoutes extends CommonRoutes {
   public constructor(app: Application) {
@@ -11,8 +9,6 @@ class VisitorRoutes extends CommonRoutes {
 
   public setUpRoutes(): Application {
     this.app.post('/visitor', createVisitorAction.run);
-    this.app.get('/visitor/command', createVisitorCommand.run);
-    this.app.put('/visitor/handler', createVisitorHandler.run);
 
     return this.app;
   }
