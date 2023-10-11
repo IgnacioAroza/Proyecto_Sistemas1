afb29c248ebc234cd0a8d7ebce1b8670135c041e
Ignacio Aroza <ignacioaroza.ia@gmail.com>
2023-10-11 11:12:45 -0300

    Fixed errors

--- server/src/http/actions/get.categories.action.ts
+++ server/src/http/actions/get.categories.action.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { getCategoriesCommand } from '../../application/commands/get.categories.command';
+import getCategoriesCommand from '../../application/commands/get.categories.command';
 import getCategoriesHandler from '../../application/handlers/get.categories.handler';
 
 class CreateClaimAction {
@@ -13,7 +13,7 @@ class CreateClaimAction {
         return res.status(201).json({ message: 'Claim created successfully' });
       } catch (error) {
         const { message } = error as Error;
-        res.status(400).json({ message: message });
+        return res.status(400).json({ message: message });
       }
     }
   }
