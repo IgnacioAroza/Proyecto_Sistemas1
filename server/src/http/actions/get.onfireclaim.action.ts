d3556d96df11f40437ed6958589f7f5756a7340e
Omar Moyano <omarmoyano7w@gmail.com>
2023-10-23 20:50:12 -0300

    method implementation

--- server/src/http/actions/get.onfireclaim.action.ts
+++ server/src/http/actions/get.onfireclaim.action.ts
@@ -1,20 +1,7 @@
-import { Request, Response } from 'express';
+import claimRepository from '../../infrastructure/repositories/claim.repository';
 
+class getOnFireClaimAction {
+  static async run(){ return claimRepository.onFireLastHour(); }
+}
 
-class CreateOnFireClaimAction {
-    async run(req: Request, res: Response) {
-      const {  } = req.body;
-  
-      try {
-        //const command = new CreateOnFireClaimAction();
-        //await createOnFireClaimHandler.execute(command);
-  
-        return res.status(201).json({ message: 'Claim created successfully' });
-      } catch (error) {
-        const { message } = error as Error;
-        return res.status(400).json({ message: message });
-      }
-    }
-  }
-  
-  export default new CreateOnFireClaimAction();
+export default new getOnFireClaimAction();
