65eca40fe568ca2c23974586df74862e5f57a795
Agustin Camejo <agustincamejo03@gmail.com>
2023-11-06 12:35:55 -0300

    Finished methods

--- server/src/http/actions/get.onfireclaim.action.ts
+++ server/src/http/actions/get.onfireclaim.action.ts
@@ -1,7 +1,17 @@
-import claimRepository from '../../infrastructure/repositories/claim.repository';
+import { Response } from "express";
+import claimRepository from "../../infrastructure/repositories/claim.repository";
 
 class getOnFireClaimAction {
-  static async run(){ return claimRepository.onFireLastHour(); }
+  public async run(res: Response) {
+    try {
+      const onFireClaims = await claimRepository.onFireLastHour();
+
+      res.status(200).json(onFireClaims);
+    } catch (error) {
+      const { message } = error as Error;
+      res.status(400).json({ message: message });
+    }
+  }
 }
 
 export default new getOnFireClaimAction();
--- server/src/infrastructure/repositories/claim.repository.ts
+++ server/src/infrastructure/repositories/claim.repository.ts
@@ -26,11 +26,30 @@ class ClaimRepository{
     let lastHour = new Date;
     lastHour.setHours(lastHour.getHours() - 1);
 
-    const recentClaims = this.claims.filter(claim => claim.getCreatedAt() >= lastHour);
+    const recentClaims = this.claims.filter(claim => claim.getCreatedAt().getTime() >= lastHour.getTime());
 
-    recentClaims.sort((a, b) => b.getLike() - a.getLike());
+    const orderClaims = recentClaims.sort((a, b) => b.getLike() - a.getLike());
     
-    return recentClaims.slice(0, 4);
+    return orderClaims.slice(0, 5);
+  }
+
+  public async lastFiveClaimPost(): Promise<Array<Claim>> {
+    const recentClaims = this.claims.sort((a, b) => b.getCreatedAt().getTime() - a.getCreatedAt().getTime());
+    
+    const orderClaims = recentClaims.slice(0, 5);
+    return orderClaims;
+  }
+
+  public async listLastFiveClaimsByVisitor(claim: Claim): Promise<Array<Claim>| null> {
+    this.claims.forEach( e => {
+      if(claim.getOwner().getId() == e.getOwner().getId()){
+        return this.claims.slice(-5);
+      }
+      else{
+        return null;
+      }
+    });
+    return this.claims;
   }
 }
 
