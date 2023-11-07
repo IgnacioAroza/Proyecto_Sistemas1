95ee65af6b4dd32900d31c369b25f7a8868fde23
Ignacio Aroza <ignacioaroza.ia@gmail.com>
2023-11-06 22:08:21 -0300

    Add visitor routes

--- server/src/http/routes/visitor.routes.ts
+++ server/src/http/routes/visitor.routes.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import CommonRoutes from './common.routes';
 import createVisitorAction from '../actions/create.visitor.action';
+import findVisitorAction from 'http/actions/find.visitor.action';
 
 class VisitorRoutes extends CommonRoutes {
   public constructor(app: Application) {
@@ -9,6 +10,7 @@ class VisitorRoutes extends CommonRoutes {
 
   public setUpRoutes(): Application {
     this.app.post('/visitor', createVisitorAction.run);
+    this.app.get('/visitor', findVisitorAction.run);
 
     return this.app;
   }
